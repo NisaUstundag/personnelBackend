26e52e9abf6b0e26a264ba83d4ce13340768ba2a
Nisa Üstündağ <nisaustundag070@gmail.com>
2024-07-25 12:54:24 +0300

    first commit

---
+++ src/main/java/com/yourcompany/personnel_backend/PersonnelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yourcompany.personnel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelBackendApplication.class, args);
+	}
+
+}
